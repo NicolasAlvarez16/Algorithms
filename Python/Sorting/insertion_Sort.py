974c88d03f3a3d4f9413c7558781ac6de9108c39
NicolasAlvarez16 <59146295+NicolasAlvarez16@users.noreply.github.com>
2020-11-13 11:07:31 +0000

    Sorting

--- Python/Sorting/insertion_Sort.py
+++ Python/Sorting/insertion_Sort.py
@@ -6,9 +6,9 @@ def insertion_sort(array):
     """Function to implement insertion sort algorithm"""
     for i in range(1, len(array)):
         # Looping through array once
-        value_sort = array[i] # Current number
+        value_sort = array[i] # Select the first unsorted element
         while array[i - 1] > value_sort and i > 0:
-            swap(i, i - 1, array) # Swaping to insert current number in sorted sublist
+            swap(i, i - 1, array) # Swaping other elements to the right to shift unsorted element
             i = i - 1
     return array
 
